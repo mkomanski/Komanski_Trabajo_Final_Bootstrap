95d629347188336d0f6399825cba57489fb18c98
Marcela <marcelakomanski@gmail.com>
2022-05-27 07:52:14 -0300

    proyecto para entregar

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-  
-
 
 function contacto() {
 
@@ -10,24 +8,6 @@ function contacto() {
     var comentario = document.getElementById("comentario").value;
 
     console.log ("Datos ingresados: ", nombre, apellido, email, telefono, comentario);
-
-     /*
-    if (nombre==""){
-        document.getElementById("nombre").innerHTML = "el nombre es obligatorio";
-    } else {
-        if (apellido==""){
-            document.getElementById("apellido").innerHTML = "el apellido es obligatorio";
-        }else {
-            if (email=="") {
-                document.getElementById("email").innerHTML= "el email es obligatorio";
-            } else {
-                if (telefono=="") {
-                    document.getElementById("telefono").innerHTML= "el teléfono es obligatorio";
-                }
-            }
-        }
-        }
-        */
 
     alert ("Gracias! En breve nos contactaremos.");
 
